9162eaef053a8be4a9db8fea09d91e5ad6ef6453
zgm <1254668381@qq.com>
2018-10-16 21:13:22 +0800

    Add files via upload

--- html2xlsx.py
+++ html2xlsx.py
@@ -7,7 +7,28 @@
 
 from win32com.client import Dispatch
 
-excelApp = Dispatch('Excel.Application')
-book = excelApp.Workbooks.open('html file path')
-book.SaveAs('xlsx save path' + '.xlsx', 51)
-book.close
+
+def htmltoxlsx(file_path, save_filename):
+    """convert a table in html to xlsx
+    only on Windows.
+    Args:
+        file_path: the open file absolute path,including filename and ext.
+        save_filename: want to save filename
+        eg.'F:\\Python\\test\\example.html'
+    Returns:
+        save a Excel(.xlsx) file.
+
+    error:
+        return the error infomation.
+    """
+    try:
+        excelApp = Dispatch('Excel.Application')
+        book = excelApp.Workbooks.open(file_path)
+        book.SaveAs(save_filename + '.xlsx', 51)
+        book.close
+        print("转换完成 / Converted successful.")
+    except Exception as e:
+        print(e)
+
+
+htmltoxlsx('F:\\Python\\test\\安徽example.html', 'F:\\Python\\test\\example')
